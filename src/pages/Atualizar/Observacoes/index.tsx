c228da18846dcdef55e7f228e95ca0831a4a8b5c
RA4Z <rraz639@gmail.com>
2023-11-30 15:31:37 -0300

    Permitindo deletar observações cadastradas

--- src/pages/Atualizar/Observacoes/index.tsx
+++ src/pages/Atualizar/Observacoes/index.tsx
@@ -1,4 +1,6 @@
 import styles from './Observacoes.module.scss'
+import { useState } from 'react'
+import Lixeira from 'assets/lixeira.png'
 import { Divider } from '@mui/material'
 
 interface Props {
@@ -7,13 +9,23 @@ interface Props {
 }
 
 export default function Observacoes(props: Props) {
+    const [obs, setObs] = useState(props.observacoes)
+
+    function deletarObs(index: number) {
+        setObs(props.observacoes.filter((_, i) => i !== index))
+        props.observacoes.splice(index, 1)
+    }
+
     return (
         <>
             <div className={styles.overlay} onClick={() => props.visible(false)} />
             <div className={styles.container}>
-                {props.observacoes.map(obs => (
-                    <div className={styles.container__card}>
-                        <li>{obs.data}</li>
+                {obs.map((obs, index) => (
+                    <div className={styles.container__card} key={index}>
+                        <div className={styles.container__date}>
+                            <li>{obs.data}</li>
+                            <img src={Lixeira} alt='Lixeira' title='Deletar Observação' onClick={() => deletarObs(index)} />
+                        </div>
                         <li>{obs.ocorrido}</li>
                         <Divider style={{ background: 'white', width: '100%', marginBottom: 25, marginTop: 20 }} />
                     </div>
