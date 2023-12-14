6529a88fd4862e2974c30555e92043198874e07e
RA4Z <rraz639@gmail.com>
2023-12-14 14:41:17 -0300

    Ordenando datas nas observações

--- src/pages/Projeto/Observacoes/index.tsx
+++ src/pages/Projeto/Observacoes/index.tsx
@@ -8,11 +8,12 @@ interface Props {
 }
 
 export default function Observacoes(props: Props) {
+    const observer = props.observacoes.sort((a, b) => (dayjs(a.data) < dayjs(b.data)) ? -1 : 1)
     return (
         <>
             <div className={styles.overlay} onClick={() => props.visible(false)} />
             <div className={styles.container}>
-                {props.observacoes.map(obs => (
+                {observer.map(obs => (
                     <div className={styles.container__card}>
                         <li>{dayjs(obs.data).format('DD/MM/YYYY')} ----- {obs.horaInicio} até {obs.horaFim}</li>
                         <li>{obs.ocorrido}</li>
