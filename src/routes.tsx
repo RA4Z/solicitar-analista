c43ea33a9471374fc4069e3ea83bdeedbb74d8a8
RA4Z <rraz639@gmail.com>
2023-12-14 16:46:40 -0300

    Adicionando Lazy Load para otimização básica no carregamento de páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react'
+import { useState, lazy, Suspense } from 'react'
 import Visualizar from "pages/Visualizar";
 import Header from "components/Header";
 import Footer from "components/Footer";
-import Cadastrar from "pages/Cadastrar";
-import Projeto from "pages/Projeto";
-import Atualizar from "pages/Atualizar";
+import { Box, LinearProgress } from "@mui/material";
+
+const Cadastrar = lazy(() => import('pages/Cadastrar'));
+const Projeto = lazy(() => import('pages/Projeto'));
+const Atualizar = lazy(() => import('pages/Atualizar'));
 
 export default function AppRouter() {
     const [viewForm, setViewForm] = useState('list')
@@ -15,14 +17,18 @@ export default function AppRouter() {
     }
     return (
         <Router>
-            <Routes>
-                <Route path='/' element={<Header formato={formato} />}>
-                    <Route index element={<Visualizar view={viewForm} />} />
-                    <Route path='/Cadastro' element={<Cadastrar />} />
-                    <Route path='/Projeto/:id' element={<Projeto />} />
-                    <Route path='/Atualizar/:id' element={<Atualizar />} />
-                </Route>
-            </Routes>
+            <Suspense fallback={<Box sx={{ width: '100%' }}>
+                <LinearProgress />
+            </Box>}>
+                <Routes>
+                    <Route path='/' element={<Header formato={formato} />}>
+                        <Route index element={<Visualizar view={viewForm} />} />
+                        <Route path='/Cadastro' element={<Cadastrar />} />
+                        <Route path='/Projeto/:id' element={<Projeto />} />
+                        <Route path='/Atualizar/:id' element={<Atualizar />} />
+                    </Route>
+                </Routes>
+            </Suspense>
             <Footer />
         </Router>
     )
